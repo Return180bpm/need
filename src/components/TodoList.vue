51b6b1114fbba03b2bd1c17469a5e0df99a998c4
Tom Szwaja <th.szwaja@gmail.com>
2023-08-28 19:20:50 +0200

    setup first xstate machine

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
+import { useMachine } from '@xstate/vue'
 import { useItemsStore } from '~/stores/items'
+import { toggleMachine } from '~/machines/toggleMachine'
+import DevLog from '~/utils/DevLog.vue'
 
 const itemsStore = useItemsStore()
 const newItemLabel = ref('')
+const { state, send } = useMachine(toggleMachine)
 
 function onAddNewItem(label: string) {
   itemsStore.add(label)
   newItemLabel.value = ''
 }
+
+function onClick(itemId: number) {
+  send('TOGGLE')
+}
 </script>
 
 <template>
-  <div class="grid gap-1">
+  <div class="grid gap-1" @click="onClick(3)">
     <form @submit.prevent="onAddNewItem(newItemLabel)">
-      <input v-model="newItemLabel" type="text" placeholder="need" class="border-2 border-light-50 rounded-full bg-teal-200 px-4 py-4 focus:outline-green-400">
+      <input v-model="newItemLabel" type="text" placeholder="need" class="w-full border-2 border-light-50 rounded-full bg-teal-200 px-4 py-4 focus:outline-green-400">
     </form>
     <ul class="grid w-full gap-1 rounded-3xl">
       <TodoItem v-for="(item, index) in itemsStore.sortedItemsTime" :key="index" :label="item.label" />
     </ul>
   </div>
+  <DevLog :content="state.value" />
 </template>
---
+++ src/utils/DevLog.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { StateTypes } from 'xstate'
+
+const { content } = defineProps<{
+  content: StateTypes
+}>()
+</script>
+
+<template>
+  <div class="dev">
+    <p>
+      {{ content }}
+    </p>
+  </div>
+</template>
